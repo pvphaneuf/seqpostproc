fbf98a31dab8840cf1c2697460886f7747f9677f
pvphaneuf <pphaneuf@eng.ucsd.edu>
2017-04-11 20:45:39 -0700

    checks if files exist

--- seqpostproc/rename_samples.py
+++ seqpostproc/rename_samples.py
@@ -1,9 +1,12 @@
 import os
+import os.path
 import csv
 
 # Expecting a CSV with each line as: #,#,#,#,current_name
 with open("mapping.csv") as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',')
     for row in spamreader:
-        new_dir_name = row[0]+'-'+row[1]+'-'+row[2]+'-'+row[3]
-        os.rename(row[4],new_dir_name)
+        current_file_name = row[4]
+        if os.path.isdir(current_file_name):
+            new_dir_name = row[0]+'-'+row[1]+'-'+row[2]+'-'+row[3]
+            os.rename(current_file_name,new_dir_name)
